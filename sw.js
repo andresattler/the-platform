1445ad4135cc19db41ed06c1adc8e75f06cc7339
Andre Sattler <andresattler.webdev@gmail.com>
2019-02-02 22:42:18 +0100

    added basic sw

---
+++ sw.js
@@ -0,0 +1,40 @@
+const VERSION = '0' // neds to be updated wit each release that invalidates chache
+// TODO some script for this
+
+oninstall = evt => {
+  evt.waitUntil(
+    caches.open(VERSION)
+      .then(cache => cache.addAll([
+        './style.css',
+        './index.js',
+        './components/tp-header/index.js',
+        './components/tp-header/style.css',
+      ]))
+  )
+}
+
+onactivate = evt => {
+  evt.waitUntil(
+    caches.keys()
+      .then((keys) =>
+        Promise.all(
+          keys.map(key => {
+            if (key !== VERSION) return caches.delete(key)
+          }) 
+        )
+      )
+  )
+}
+
+onfetch = evt => {
+  evt.waitUntil(
+    caches.match(evt.request)
+      .then(match => {
+        if (match) {
+          console.log('match', evt.request)
+          return match 
+        }
+        return fetch(evt.request)
+      })
+  )
+}
